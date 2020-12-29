fa259bb7e75cbadca84c6ca1d15a5b39cd39f6e5
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-12-29 21:58:45 +0200

    slider works

--- public/js/index.js
+++ public/js/index.js
@@ -12,58 +12,129 @@ const SLIDES_ARR = [
   {
     heading: 'Vestibulium',
     description: 'Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.',
-    imgUrl: 'https://look.com.ua/pic/201806/2560x1600/look.com.ua-286462.jpg'
+    imgUrl: 'https://canadalifechurch.com/wp-content/uploads/2017/06/2017.02.05.jpg'
   },
 ]
 
-const slider = new Slider(sliderContainer, SLIDES_ARR);
+const mainSlider = new Slider(sliderContainer, SLIDES_ARR);
 
-console.log(slider)
+const sliderButtonNext = document.getElementById('slider__next');
+const sliderButtonPrev = document.getElementById('slider__prev');
+
+sliderButtonNext.addEventListener('click', ()=>{
+  mainSlider.next();
+});
+
+sliderButtonPrev.addEventListener('click', ()=>{
+  mainSlider.prev();
+});
 },{"./scripts/slider":2}],2:[function(require,module,exports){
 "use strict";
 
 module.exports = class Slider {
   constructor(container, slides, slidesOnScreen = 1) {
     this.container = container;
-    this.slides = slides;
     this.slidesOnScreen = slidesOnScreen;
     
-    this._slidesDataArr = [];
-    this.width = this.container.clientWidth;
-    this.height = this.container.clientHeight;
+    this.slidesElements = [];
+    this.currentSlide = 0;
+    this.width = 0;
+    this.height = 0;
 
-    this.init();
+    this.init(slides);
   }
 
-  init() {
-    this.container.style.overflow = "hidden";
-    this._initSlidesData();
-    this.container.innerHTML =
-      `<div style="position:relative; height:${this.height}px; width:${this._calcImagesWrappWidth()}px">${this._createImages()}</div>`;
+  init(slides) {
+    this._updateSizes()
+    this.slides = this._returnSlidesData(slides);
+    // this._updateCurrentSlide();
+    this._createImages();
+    this._drawSlides();
+   
+  }
+
+  _updateSizes() {
+    this.width = this.container.clientWidth;
+    this.height = this.container.clientHeight;
+  }
+
+  _updateCurrentSlide() {
+    this.currentSlide = this.slides.findIndex((el)=>el.isOnScreen);
   }
 
   next() {
-    
+    this.setSlide(this.currentSlide + 1);    
   }
 
-  _initSlidesData() {
-    this._slidesDataArr = this.slides.map((el, i)=> {
+  prev() {
+    this.setSlide(this.currentSlide - 1);
+  }
+
+
+  setSlide(index) {
+    if (index < 0 || index > this.slides.length-1) return;
+    
+    const scrollWidth = this.slides[index].position;
+
+    this.slides = this.slides.map((el) => {
+      if (this.currentSlide > index) {
+        el.position -= scrollWidth;
+      } else el.position -= scrollWidth;
+
+      el.isOnScreen = false;
+      return el;
+    });
+
+    this.currentSlide = index;
+
+    this._updateSlides();
+  }
+
+  _returnSlidesData(slidesArr) {
+    return slidesArr.map((el, i)=> {
       el.position = i * this.width;
       el.isOnScreen = (i === 0);
+      
       return el;
     });
   }
 
   _createImages() {
-    return this.slides.map((el, i) =>
-          `<img src="${el.imgUrl}" style="
-          position: absolute;
-          left: ${this._slidesDataArr[i].position}px;
-          width:${this._calcImagesWidth()}px;
-          object-fit:cover;
-          height:inherit;
-          transition:all 1s ease">`)
-          .join("");
+    this.slides.forEach((el)=>{
+      this.slidesElements.push(
+        this._returnSlideElement(el)
+      );
+    });
+  }
+
+  _drawSlides() {
+    this.container.style.overflow = "hidden";
+    this.container.style.position = 'relative';
+    this.container.style.height = this.height;
+    this.container.style.width = this._calcImagesWrappWidth();
+    
+    this.container.append(...this.slidesElements);
+  }
+
+  _updateSlides() {
+    this.slidesElements.forEach((el, i) => 
+      el.style.left = this.slides[i].position + 'px');
+  }
+
+
+  _returnSlideElement({imgUrl, position}) {
+    const img = document.createElement('img');
+    img.src = imgUrl;
+
+    img.style.position = 'absolute';
+    img.style.top = 0;
+    img.style.objectFit = 'none';
+    img.style.height = 'inherit';
+    img.style.left = position + 'px';
+    img.style.transition = 'all 1s ease';
+    img.style.width = this._calcImagesWidth()+'px';
+    
+    return img;
   }
 
   _calcImagesWidth() {
--- src/index.js
+++ src/index.js
@@ -11,10 +11,19 @@ const SLIDES_ARR = [
   {
     heading: 'Vestibulium',
     description: 'Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.',
-    imgUrl: 'https://look.com.ua/pic/201806/2560x1600/look.com.ua-286462.jpg'
+    imgUrl: 'https://canadalifechurch.com/wp-content/uploads/2017/06/2017.02.05.jpg'
   },
 ]
 
-const slider = new Slider(sliderContainer, SLIDES_ARR);
+const mainSlider = new Slider(sliderContainer, SLIDES_ARR);
 
-console.log(slider)
+const sliderButtonNext = document.getElementById('slider__next');
+const sliderButtonPrev = document.getElementById('slider__prev');
+
+sliderButtonNext.addEventListener('click', ()=>{
+  mainSlider.next();
+});
+
+sliderButtonPrev.addEventListener('click', ()=>{
+  mainSlider.prev();
+});
