78890a024d4019b5da96ef6c6d94dcd7e974cc8c
Firdovsi Mamedov <firdovsim96@gmail.com>
2020-12-30 19:36:11 +0200

    realy

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-'use strict';
-
 // const module = require('./scripts/script.js'); <-- пример подключения скрипта из папки scripts
 
-const header = require('./scripts/header.js');
-
-
-module.export = header
-},{"./scripts/header.js":2}],2:[function(require,module,exports){
 'use strict'
 
 
@@ -24,19 +17,14 @@ window.addEventListener('scroll', () => {
 
 
 
-let toggle = document.querySelector(".burger");
-toggleHandler(toggle)
+const toggle = document.querySelector(".burger")
+    .addEventListener("click", function (e) {
+    e.preventDefault();
 
-
-function toggleHandler(toggle) {
-    toggle.addEventListener("click", function (e) {
-        e.preventDefault();
-
-            if (this.classList.contains('active')) {
-                this.classList.remove("active")
-            } else {
-                this.classList.add("active");
-            }
-     });
-}
+    if (this.classList.contains('active')) {
+        this.classList.remove("active")
+    } else {
+        this.classList.add("active");
+    }
+});
 },{}]},{},[1]);
