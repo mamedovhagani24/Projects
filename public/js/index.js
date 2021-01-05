53f47fe0229111ebdc81566bd80412576136ca70
Максим Мамедов <frondenddeveloper@gmail.com>
2021-01-05 16:37:49 +0200

    control

--- public/js/index.js
+++ public/js/index.js
@@ -55,7 +55,7 @@ function createMainSliderMarkers(slides) {
     marker.setAttribute("data-index", i);
 
     marker.classList.add("slider__control-item");
-    
+
     if (i === 0) marker.classList.add("slider__control-item_active");
 
     return marker;
@@ -82,25 +82,24 @@ sliderButtonPrev.addEventListener("click", () => {
 
 
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
+  const header = document.querySelector('.header');
 
-    if (window.pageYOffset > 200) {
-        header.classList.add('header__small');
-    } else {
-        header.classList.remove('header__small');
-    }
+  if (window.pageYOffset > 200) {
+    header.classList.add('header__small');
+  } else {
+    header.classList.remove('header__small');
+  }
 });
 
 const toggle = document.querySelector(".burger")
-    .addEventListener("click", function (e) {
-    e.preventDefault();
+  .addEventListener("click", function (e) {
+    const header__burger = document.querySelector('.header__menu')
 
-    if (this.classList.contains('active')) {
-        this.classList.remove("active")
-    } else {
-        this.classList.add("active");
-    }
-});
+    e.preventDefault();
+    this.classList.toggle("active");
+    header__burger.classList.toggle('active');
+
+  });
 
 },{"./scripts/services-drag-n-drop.js":2,"./scripts/slider":3}],2:[function(require,module,exports){
 function enableDragSort(listClass) {
