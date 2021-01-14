6a89d1254b71223ff63531a97c14b617dd5741cd
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-14 03:08:55 +0200

    header slide

--- public/js/index.js
+++ public/js/index.js
@@ -69,7 +69,7 @@ function updateClientsSliderButtons(currSlide) {
 window.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
 
-    if (window.pageYOffset > 200 && !header.classList.contains('header-mobile')) {
+    if (window.pageYOffset > 200 && !header.classList.contains('header-mobile_open')) {
         header.classList.add('header__small');
     } else {
         header.classList.remove('header__small');
@@ -78,11 +78,21 @@ window.addEventListener('scroll', () => {
 
 const toggle = document.querySelector(".burger")
     .addEventListener("click", function (e) {
-        const header__burger = document.querySelector('.header__menu')
+        const header__burger = document.querySelector('.header')
 
         e.preventDefault();
         this.classList.toggle("active");
-        header__burger.classList.toggle('active');
+
+        if (header__burger.classList.contains('header-mobile_open')){
+            header__burger.classList.remove('header-mobile_open')
+            header__burger.classList.add('header-mobile_closing');
+
+            setTimeout(() => {
+                header__burger.classList.remove('header-mobile_closing');                
+            }, 1000);
+        } else {
+            header__burger.classList.add('header-mobile_open');
+        }
 
     });
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
 
-    if (window.pageYOffset > 200 && !header.classList.contains('header-mobile')) {
+    if (window.pageYOffset > 200 && !header.classList.contains('header-mobile_open')) {
         header.classList.add('header__small');
     } else {
         header.classList.remove('header__small');
@@ -10,11 +10,21 @@ window.addEventListener('scroll', () => {
 
 const toggle = document.querySelector(".burger")
     .addEventListener("click", function (e) {
-        const header__burger = document.querySelector('.header__menu')
+        const header__burger = document.querySelector('.header')
 
         e.preventDefault();
         this.classList.toggle("active");
-        header__burger.classList.toggle('active');
+
+        if (header__burger.classList.contains('header-mobile_open')){
+            header__burger.classList.remove('header-mobile_open')
+            header__burger.classList.add('header-mobile_closing');
+
+            setTimeout(() => {
+                header__burger.classList.remove('header-mobile_closing');                
+            }, 1000);
+        } else {
+            header__burger.classList.add('header-mobile_open');
+        }
 
     });
 
