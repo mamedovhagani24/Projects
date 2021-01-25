d8f031f4549bf0b0680e49d347c5e1777823d67e
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-25 03:52:17 +0200

    slider move prev, next & buttons disabling

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,11 +2,32 @@ import Slider from "../../scripts/Slider";
 
 const carousel = document.getElementById("carousel");
 
+const sliderControls = document.querySelectorAll('.slider__btn');
+
 const slider = new Slider({
   container: carousel,
   slidesClass: ".carousel__slide",
-  headerClass: ".carousel__slide",
-  descriptionClass: ".carousel__slide",
+  titleClass: ".carousel__slide-title",
+  descriptionClass: ".carousel__slide-description",
 });
 
 slider.init();
+
+sliderControls.forEach((btn)=>{
+  const btnName = btn.id.split('__')[1];
+
+  btn.addEventListener('click', ()=>{
+    slider[btnName]();
+    checkButtonsDisabling();
+  });
+});
+checkButtonsDisabling();
+
+slider.adaptive.onResize((e)=>{
+  
+});
+
+function checkButtonsDisabling() {
+  sliderControls[0].disabled = slider.activeSlide === 0;
+  sliderControls[1].disabled = slider.activeSlide === slider.elements.length - 1;
+}
--- src/scripts/Slider.js
+++ src/scripts/Slider.js
@@ -1,17 +1,52 @@
 export default class Slider {
-  constructor({container, slidesClass}) {
-    this.slidesDom = new sliderDOM(container, slidesClass);
+  constructor(data) {
+    this.sliderDom = new sliderDOM(data);
+    this.adaptive = new sliderAdaptive();
+
+    this.elements = {};
+    this.activeSlide = 0;
   }
-  
+
   init() {
-    this.slidesDom.init();
+    this.sliderDom.init();
+    this.adaptive.init();
+    
+    this.updateElementsInfo();
+    // this.adaptive.onResize(this.updateElementsInfo.bind(this));
+  }
+
+  updateElementsInfo(e) {
+    this.elements = this.sliderDom.getInfoFromElements();
+  }
+
+  next() {
+    this.setSlide(++this.activeSlide);
+  }
+
+  prev() {
+    this.setSlide(--this.activeSlide);
+  }
+
+  setSlide(index) {
+    index = (index<0) ? 0 : 
+            (index>=this.elements.length) ? 
+            this.elements.length-1 : index;
+
+    const shift = this.elements[index].position;
+
+    this.activeSlide = index;
+    this.sliderDom.moveElements(-shift);
   }
 }
 
+
 class sliderDOM {
-  constructor(container, slidesClass) {
+  constructor({ container, slidesClass, titleClass, descriptionClass }) {
     this.container = container;
     this.slidesClass = slidesClass;
+    this.titleClass = titleClass;
+    this.descriptionClass = descriptionClass;
+
     this.elements = [];
   }
 
@@ -19,7 +54,33 @@ class sliderDOM {
     this.elements = this.container.querySelectorAll(this.slidesClass);
   }
 
-  getElementsData() {
-    // return 
+  getInfoFromElements() {
+    return [...this.elements].map((el, id) => {
+      const { width, x: position } = el.getBoundingClientRect();
+      return { id, width, position };
+    });
+  }
+
+  moveElements(positionX) {
+    this.container.style.transform = `translateX(${positionX}px)`;
+  }
+}
+
+
+class sliderAdaptive {
+  constructor() {
+    this.callbacks = [];
+  }
+
+  init() {
+    window.addEventListener("resize", this.initResize.bind(this));
+  }
+
+  initResize(event) {
+    this.callbacks.forEach((callback)=>callback(event));
+  }
+
+  onResize(callback) {
+    this.callbacks.push(callback);
   }
 }
