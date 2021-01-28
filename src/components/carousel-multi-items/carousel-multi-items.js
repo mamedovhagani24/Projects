286e87313ef3eaf62b64f83056740d8d12919667
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-28 02:04:20 +0200

    slider works... mabey

--- src/components/carousel-multi-items/carousel-multi-items.js
+++ src/components/carousel-multi-items/carousel-multi-items.js
@@ -1,57 +1,70 @@
-import SliderMultiItems from '../../scripts/MultiCarousel';
+import SliderMultiItems from "../../scripts/MultiCarousel";
 
-const multiSliderContainer = document.getElementById('carousel-multi');
-const navBtns = document.querySelectorAll('.about-us__nav-btn');
-const sliderRange = document.getElementById('slider-range');
+const multiSliderContainer = document.getElementById("carousel-multi");
+const navBtns = document.querySelectorAll(".about-us__nav-btn");
+const sliderRange = document.getElementById("slider-range");
 
 const multiSlider = new SliderMultiItems({
   container: multiSliderContainer,
   slidesClass: ".carousel_multi__slide",
-  slideTime: .5,
-  slidesOnScreen: 6
+  slideTime: 0.5,
+  slidesOnScreen: 6,
 });
 
-setSlidesOnScreenByContainerWidth();
+multiSlider.init(function () {
+  setAmountSlidesByContainerWidth();
+  setRangeAttributes();
 
-multiSlider.init();
+  multiSlider.events.onSlideChange = slideChanged;
+});
 
-setRangeAttributes();
-function setRangeAttributes() {
-  sliderRange.setAttribute('min', '0');
-  sliderRange.setAttribute('max', multiSlider.elements[multiSlider.maxSlide].position);
+function slideChanged(index) {
+  checkButtonsDisabling(index);
+  setRangeValue(index);
 }
 
-sliderRange.addEventListener('input', function() {
+function setRangeAttributes() {
+  sliderRange.setAttribute("min", "0");
+  sliderRange.setAttribute(
+    "max",
+    multiSlider.elements[multiSlider.maxSlide].position
+  );
+}
+
+sliderRange.addEventListener("input", function () {
   const val = +this.value;
 
   multiSlider.activeTransition(false);
   multiSlider.sliderDom.moveElements(-val);
 });
 
-sliderRange.addEventListener('change', function(){
+sliderRange.addEventListener("change", function () {
   const nextSliderId = multiSlider.nextSlideIndexByPosition;
   multiSlider.activeTransition(true);
   multiSlider.setSlide(nextSliderId);
+  setRangeValue(nextSliderId);
 });
 
-// sliderRange.addEventListener()
+function setRangeValue(index) {
+  sliderRange.value = multiSlider.elements[index].position;
+}
 
 navBtns.forEach((el) => {
-  el.addEventListener('click', () => {
-    const slideDir = el.id.split('__')[1];
+  el.addEventListener("click", () => {
+    const slideDir = el.id.split("__")[1];
 
     multiSlider[slideDir]();
-
-    checkButtonsDisabling();
   });
 });
 
-multiSlider.adaptive.onResize(setSlidesOnScreenByContainerWidth);
+multiSlider.adaptive.onResize(setAmountSlidesByContainerWidth);
 
-function setSlidesOnScreenByContainerWidth() {
-  if (multiSlider.containerWidth < 1000) multiSlider.slidesOnScreen = 4;
+function setAmountSlidesByContainerWidth() {
   if (multiSlider.containerWidth < 700) multiSlider.slidesOnScreen = 2;
+  else if (multiSlider.containerWidth < 1000) multiSlider.slidesOnScreen = 4;
   else multiSlider.slidesOnScreen = 6;
+  
+  setRangeAttributes();
 }
 
 checkButtonsDisabling();
